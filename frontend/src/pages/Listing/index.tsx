a8f927076fe97968333a75f7448f236757d95814
Eduardo <eduardotakeuchi@outlook.com>
2022-05-15 14:12:49 -0300

    First Request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,14 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/request";
 function Listing() {
+  //forma errada
+  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <Pagination />
       <div className="container">
         <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
@@ -21,7 +25,9 @@ function Listing() {
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
         </div>
       </div>
     </>
